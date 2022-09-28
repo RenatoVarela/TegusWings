dbb4c00937850f34db3d58343d9f2331f71f8045
jairosierra20 <jairosierra204@hotmail.com>
2022-09-27 20:15:26 -0600

    Arreglo en Home

--- src/Componentes/PaginaHome.js
+++ src/Componentes/PaginaHome.js
@@ -12,21 +12,15 @@ export default function PaginaHome() {
       <br></br>
       <br></br>
       <Grid container spacing={3}>
-        <Grid item xs={6} sm={6} style={{ justifyContent: "center" }}>
-          <img
-            className="d-block"
-            src="https://i.ibb.co/R3N9sKV/zyro-image-1.jpg"
-            alt="First slide"
-            style={{ width: "80%", height: "60%", marginLeft: "21%" }}
-          />
-        </Grid>
-        <Grid item xs={6} sm={6}>
-          <img
-            src="https://i.ibb.co/xzjfgdY/Whats-App-Image-2022-09-22-at-3-32-10-PM.jpg"
-            style={{ width: "80%", height: "60%", marginLeft: "8%" }}
-          ></img>
-        </Grid>
 
+      <Grid item xs={12} sm={12}>
+      <img
+      className="d-block w-100"
+      src="https://i.ibb.co/Fnwsz0v/Presentaci-n2-3.jpg"
+      alt="First slide"
+      style={{height:"80px"}}
+    />
+    </Grid>
         <div>
           <Grid item xs={6} sm={10} style={{ marginLeft: "9%" }}>
             <h1 style={{ textAlign: "center" }}>Menús</h1>
